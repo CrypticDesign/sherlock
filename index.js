f47bc9ed39d1e5b45d0f636708586e0ab981a161
CrypticDesign <mashoobee@gmail.com>
2018-06-01 17:59:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
+
 fs.readdir("./commands/", (err, files) => {
   if (err) console.log(err);
 
